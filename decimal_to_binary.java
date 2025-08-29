54121591618843579ef5dbfbb1c040a912da8253
sowmian_s <sowmisowmiyan58@gmail.com>
2025-08-29 21:35:06 +0530

    Fix variable name for Scanner in decimal to binary program

--- decimal_to_binary.java
+++ decimal_to_binary.java
@@ -3,12 +3,10 @@ import java.util.Scanner;
 public class decimal_to_binary {
     public static void main(String[] args) {
 
+        try (Scanner s = new Scanner(System.in)) {
         System.out.print("Enter a decimal number: ");
-        int n = sc.nextInt();
-
+        int n = s.nextInt();
         String binary = "";
-        
-        
         while (n > 0) {
             int remainder = n % 2;          
             binary = remainder + binary;    
@@ -16,5 +14,6 @@ public class decimal_to_binary {
         }
 
         System.out.println("Binary: " + binary);
+    }
     }
 }
